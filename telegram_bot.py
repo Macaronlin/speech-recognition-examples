c0cec9dbc13d685aa202e87bf713cbf67ce030ac
Nikita L <komesergey@gmail.com>
2021-04-03 18:08:13 +0300

    Refactoring

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,25 +1,22 @@
-import subprocess
-
+import os
 import torch
 import torchaudio
-import os
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from pydub import AudioSegment
 from telegram.ext import Updater, MessageHandler, Filters
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+
 processor = Wav2Vec2Processor.from_pretrained("anton-l/wav2vec2-large-xlsr-53-russian")
 model = Wav2Vec2ForCTC.from_pretrained("anton-l/wav2vec2-large-xlsr-53-russian").to("cuda")
 resampler = torchaudio.transforms.Resample(48_000, 16_000)
 
 
 def voice_handler(update, context):
-    file = context.bot.getFile(update.message.voice.file_id)
     src = str(update.message.voice.file_id) + '.ogg'
     dst = str(update.message.voice.file_id) + '.wav'
-    file.download(src)
-    process = subprocess.run(['ffmpeg', '-y', '-i', src, dst])
-    if process.returncode != 0:
-        raise Exception("Something went wrong")
-    speech = resampler(torchaudio.load(dst)[0]).squeeze().numpy()
-    input_values = processor(speech, sampling_rate=16_000, return_tensors="pt", padding=True).input_values
+    context.bot.getFile(update.message.voice.file_id).download(src)
+    AudioSegment.from_ogg(src).export(dst, format="wav")
+    input_values = processor(resampler(torchaudio.load(dst)[0]).squeeze().numpy(), sampling_rate=16_000,
+                             return_tensors="pt", padding=True).input_values
     with torch.no_grad():
         logits = model(input_values.to("cuda")).logits
     os.remove(src)
@@ -28,7 +25,6 @@ def voice_handler(update, context):
                              text=processor.batch_decode(torch.argmax(logits, dim=-1))[0].lower())
 
 
-updater = Updater(token='TOKEN')
-dispatcher = updater.dispatcher
-dispatcher.add_handler(MessageHandler(Filters.voice, voice_handler))
+updater = Updater(token='1180056473:AAEuNoc5GFcLHDZTae-CXSSwYZAaX909ra4')
+updater.dispatcher.add_handler(MessageHandler(Filters.voice, voice_handler))
 updater.start_polling()
